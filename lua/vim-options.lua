7b6027ae6e946032afab346a9c81e66ad5f78bab
erickGoJi <erickgomezlol96@gmail.com>
2025-03-26 09:16:11 -0600

    [feature] Add key map for copy commands

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,5 +3,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>p", '"+yy')
 vim.g.mapleader = " "
 
