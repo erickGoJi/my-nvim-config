f8e9c47e49ff1e940200e7e9df8dcc0c479b3718
erickGoJi <erickgomezlol96@gmail.com>
2025-03-30 12:07:02 -0600

    [feature] Add comments plugin

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,24 +1,23 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "kyazdani42/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
-    vim.keymap.set('n', '<leader>bf', ':Neotree buffers reveal float<CR>', {})
-    local configTree = require("neo-tree")
-    configTree.setup({
-      filesystem = {
-        filtered_items = {
-          visible = true,  -- Mostrar archivos ocultos
-          hide_dotfiles = false, -- No ocultar archivos que empiezan con `.`
-          hide_gitignored = false, -- Mostrar archivos ignorados por git
-        },
-      },
-    })
-  end  
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"kyazdani42/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+		local configTree = require("neo-tree")
+		configTree.setup({
+			filesystem = {
+				filtered_items = {
+					visible = true, -- Mostrar archivos ocultos
+					hide_dotfiles = false, -- No ocultar archivos que empiezan con `.`
+					hide_gitignored = false, -- Mostrar archivos ignorados por git
+				},
+			},
+		})
+	end,
 }
-
--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,24 +1,34 @@
 return {
-  --{
-  --	"catppuccin/nvim",
-  --	lazy = false,
-  --	name = "catppuccin",
-  --	priority = 1000,
-  --	enabled = false,
-  --	config = function()
-  --		require("catppuccin").setup({
-  --			flavour = "macchiato", -- Cambia entre latte, frappe, macchiato, mocha
-  --		})
-  --		vim.cmd.colorscheme("catppuccin")
-  --	end,
-  --},
-  {
-    "yorumicolors/yorumi.nvim",
-    lazy = false,
-    name = "yorumi",
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme("yorumi")
-    end,
-  },
+	{
+		"catppuccin/nvim",
+		lazy = false,
+		name = "catppuccin",
+		priority = 1000,
+		enabled = false,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "macchiato", -- Cambia entre latte, frappe, macchiato, mocha
+			})
+			-- vim.cmd.colorscheme("catppuccin")
+		end,
+	},
+	{
+		"yorumicolors/yorumi.nvim",
+		lazy = false,
+		name = "yorumi",
+		priority = 1000,
+		config = function()
+			-- vim.cmd.colorscheme("yorumi")
+		end,
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+		config = function()
+			vim.g.tokyonight_style = "storm"
+			vim.cmd([[colorscheme tokyonight]])
+		end,
+	},
 }
