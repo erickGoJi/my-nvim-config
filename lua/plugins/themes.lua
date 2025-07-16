5c9e3cee0a0ff5d03b146c04d7d8baab368f0f5a
erickGoJi <erickgomezlol96@gmail.com>
2025-07-15 18:43:06 -0600

    fix: update themes.lua for better readability and organization
    - Refactored the code to improve clarity and maintainability.
    - Organized theme configurations into distinct sections.
    - Added comments for better understanding of each theme's purpose.

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,28 +1,28 @@
 return {
-	{
-		"catppuccin/nvim",
-		lazy = false,
-		name = "catppuccin",
-		priority = 1000,
-		enabled = true,
-		config = function()
-			require("catppuccin").setup({
-				integrations = {
-					cmp = true,
-					gitsigns = true,
-					nvimtree = true,
-					treesitter = true,
-					notify = true,
-					mini = {
-						enabled = true,
-						indentscope_color = "",
-					},
-				},
-				flavour = "mocha", -- Cambia entre latte, frappe, macchiato, mocha
-			})
-			vim.cmd.colorscheme("catppuccin")
-		end,
-	},
+	-- {
+	-- 	"catppuccin/nvim",
+	-- 	lazy = false,
+	-- 	name = "catppuccin",
+	-- 	priority = 1000,
+	-- 	enabled = true,
+	-- 	config = function()
+	-- 		require("catppuccin").setup({
+	-- 			integrations = {
+	-- 				cmp = true,
+	-- 				gitsigns = true,
+	-- 				nvimtree = true,
+	-- 				treesitter = true,
+	-- 				notify = true,
+	-- 				mini = {
+	-- 					enabled = true,
+	-- 					indentscope_color = "",
+	-- 				},
+	-- 			},
+	-- 			flavour = "mocha", -- Cambia entre latte, frappe, macchiato, mocha
+	-- 		})
+	-- 		vim.cmd.colorscheme("catppuccin")
+	-- 	end,
+	-- },
 	-- {
 	-- 	"yorumicolors/yorumi.nvim",
 	-- 	lazy = false,
@@ -42,4 +42,31 @@ return {
 	-- 		vim.cmd([[colorscheme tokyonight]])
 	-- 	end,
 	-- },
+	{
+		"akinsho/horizon.nvim",
+		version = "*",
+		opts = {
+			plugins = {
+				cmp = true,
+				indent_blankline = true,
+				nvim_tree = true,
+				telescope = true,
+				which_key = true,
+				barbar = true,
+				notify = true,
+				symbols_outline = true,
+				neo_tree = true,
+				gitsigns = true,
+				crates = true,
+				hop = true,
+				navic = true,
+				quickscope = true,
+				flash = true,
+			},
+		},
+		config = function()
+			require("horizon").setup({})
+			vim.cmd.colorscheme("horizon")
+		end,
+	},
 }
