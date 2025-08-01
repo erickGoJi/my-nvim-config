8ed6a4cc3f6194fdac06d4e29e7e363a5c4976ac
erickGoJi <erickgomezlol96@gmail.com>
2025-08-01 12:07:57 -0600

    [feature] improve aute sessions manager, mapping

--- lua/plugins/sessions.lua
+++ lua/plugins/sessions.lua
@@ -8,6 +8,19 @@ return {
 				auto_session_enable_last_session = true,
 				auto_save_enabled = true,
 				auto_restore_enabled = true,
+				session_lens = {
+					buftype_filter = {},
+					load_on_setup = true,
+					theme_conf = { border = true },
+					previewer = false,
+				},
+				vim.keymap.set("n", "<leader>ss", "<cmd>SessionSave<cr>", { desc = "Save Session" }),
+				vim.keymap.set(
+					"n",
+					"leader>ls",
+					require("auto-session.session-lens").search_session,
+					{ noremap = true, silent = true, desc = "Search Sessions" }
+				),
 			})
 		end,
 	},
