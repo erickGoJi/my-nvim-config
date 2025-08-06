52543743e8c0cb6ff5881afb11ae5602fa2d2ace
erickGoJi <erickgomezlol96@gmail.com>
2025-08-06 12:56:31 -0600

    [feature] Add bar plugin and update lazy-lock.json

---
+++ lua/plugins/bar.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "romgrk/barbar.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+      "lewis6991/gitsigns.nvim",
+    },
+    init = function()
+      vim.g.barbar_auto_setup = false
+    end,
+    opts = {
+      -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+      -- animation = true,
+      -- insert_at_start = true,
+      -- …etc.
+    },
+    config = function()
+      require("barbar").setup({})
+      local map = vim.keymap.set
+      local opts = { noremap = true, silent = true }
+
+      -- Navegar buffers
+      map("n", "<Tab>", "<Cmd>BufferNext<CR>", opts)
+      map("n", "<S-Tab>", "<Cmd>BufferPrevious<CR>", opts)
+
+      -- Mover buffer a la izquierda o derecha
+      map("n", "<A-Left>", "<Cmd>BufferMovePrevious<CR>", opts)
+      map("n", "<A-Right>", "<Cmd>BufferMoveNext<CR>", opts)
+
+      -- Cerrar buffer actual
+      map("n", "<leader>c", "<Cmd>BufferClose<CR>", opts)
+    end,
+    version = "^1.0.0", -- optional: only update when a new 1.x version is released
+  },
+}
