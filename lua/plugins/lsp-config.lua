09aefb83305b4ca79c23d8519b1e0e3d9b1bc8f8
Erick <eegomezj@gbm.com.mx>
2025-08-06 13:03:56 -0600

    [feature] Update Copilot, LSP Config, and Sessions plugins

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,84 +1,84 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			auto_install = true,
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.gopls.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.gopls.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.ts_ls.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.dockerls.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.dockerls.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.erlangls.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.erlangls.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.gleam.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.gleam.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.bashls.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.bashls.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.yamlls.setup({
-				capabilities = capabilities,
-				settings = {
-					yaml = {
-						format = {
-							enable = true,
-						},
-						validate = true,
-						schemaStore = {
-							enable = true,
-							url = "https://www.schemastore.org/api/json/catalog.json",
-						},
-						schemas = {
-							["https://raw.githubusercontent.com/lalcebo/json-schema/master/serverless/reference.json"] = {
-								"serverless.yml",
-								"serverless.yaml",
-							},
-							["https://raw.githubusercontent.com/SchemaStore/schemastore/master/src/schemas/json/github-workflow.json"] = {
-								".github/workflows/*",
-							},
-						},
-					},
-				},
-			})
+      lspconfig.yamlls.setup({
+        capabilities = capabilities,
+        settings = {
+          yaml = {
+            format = {
+              enable = true,
+            },
+            validate = true,
+            schemaStore = {
+              enable = true,
+              url = "https://www.schemastore.org/api/json/catalog.json",
+            },
+            schemas = {
+              ["https://raw.githubusercontent.com/SchemaStore/schemastore/master/src/schemas/json/github-workflow.json"] = {
+                ".github/workflows/*",
+              },
+              ["https://json.schemastore.org/composer"] = {
+                "serverless.yml",
+                "serverless.yaml",
+              },
+            },
+          },
+        },
+      })
 
-			lspconfig.pyright.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.pyright.setup({
+        capabilities = capabilities,
+      })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
--- lua/plugins/sessions.lua
+++ lua/plugins/sessions.lua
@@ -1,23 +1,23 @@
 return {
-  {
-    "rmagatti/auto-session",
-    config = function()
-      require("auto-session").setup({
-        auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "~/fork" },
-        session_lens = {
-          buftypes_to_ignore = {},
-          load_on_setup = true,
-          theme_conf = { border = true },
-          previewer = false,
-        },
-        vim.keymap.set("n", "<Leader>ss", "<cmd>SessionSave<cr>", { desc = "Save Session" }),
-        vim.keymap.set(
-          "n",
-          "<Leader>ls",
-          require("auto-session.session-lens").search_session,
-          { noremap = true, silent = true, desc = "Search Sessions" }
-        ),
-      })
-    end,
-  },
+	{
+		"rmagatti/auto-session",
+		config = function()
+			require("auto-session").setup({
+				auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "~/fork" },
+				session_lens = {
+					buftypes_to_ignore = {},
+					load_on_setup = true,
+					theme_conf = { border = true },
+					previewer = false,
+				},
+				vim.keymap.set("n", "<Leader>ss", "<cmd>SessionSave<cr>", { desc = "Save Session" }),
+				vim.keymap.set(
+					"n",
+					"<Leader>ls",
+					require("auto-session.session-lens").search_session,
+					{ noremap = true, silent = true, desc = "Search Sessions" }
+				),
+			})
+		end,
+	},
 }
