ffc029e8d7df8779e38428cd01a89409d6994e98
erickGoJi <erickgomezlol96@gmail.com>
2025-08-11 10:20:39 -0600

    [enhancement] Add LSP and formatting configurations

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -151,6 +151,18 @@ return {
 
 			lspconfig.pyright.setup({
 				capabilities = capabilities,
+				settings = {
+					pyright = {
+						-- Using Ruff's import organizer
+						disableOrganizeImports = true,
+					},
+					python = {
+						analysis = {
+							-- Ignore all files for analysis to exclusively use Ruff for linting
+							ignore = { "*" },
+						},
+					},
+				},
 			})
 
 			lspconfig.ruff.setup({
