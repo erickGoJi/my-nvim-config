4fd3ccbcbcfacc43b42c15e4b6477687a11b6f0c
erickGoJi <erickgomezlol96@gmail.com>
2025-08-26 11:22:27 -0600

    [feat] add helpview plugin and update copilot configuration, fix themes and none-ls setup

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,33 +1,39 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		local augrup = vim.api.nvim_create_augroup("LspFormatting", {})
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.gofumpt,
-				null_ls.builtins.formatting.goimports_reviser,
-				null_ls.builtins.diagnostics.golangci_lint,
-				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.formatting.yamlfmt,
-				null_ls.builtins.formatting.black,
-				null_ls.builtins.formatting.ruff,
-			},
-			on_attach = function(client, bufnr)
-				if client:supports_method("textDocument/formatting") then
-					vim.api.nvim_clear_autocmds({ group = augrup, buffer = bufnr })
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						group = augrup,
-						buffer = bufnr,
-						callback = function()
-							vim.lsp.buf.format({ bufnr = bufnr })
-						end,
-					})
-				end
-			end,
-		})
-
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+  {
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+      "nvimtools/none-ls-extras.nvim",
+    },
+    config = function()
+      local null_ls = require("null-ls")
+      local augrup = vim.api.nvim_create_augroup("LspFormatting", {})
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.formatting.stylua,
+          null_ls.builtins.formatting.gofumpt,
+          null_ls.builtins.formatting.goimports_reviser,
+          null_ls.builtins.formatting.prettier,
+          null_ls.builtins.formatting.yamlfmt,
+          null_ls.builtins.formatting.black,
+          null_ls.builtins.formatting.ruff,
+          null_ls.builtins.diagnostics.golangci_lint,
+          require("none-ls.diagnostics.eslint"),
+        },
+        on_attach = function(client, bufnr)
+          if client:supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augrup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              group = augrup,
+              buffer = bufnr,
+              callback = function()
+                vim.lsp.buf.format({ bufnr = bufnr })
+              end,
+            })
+          end
+        end,
+      })
+      vim.lsp.buf.format({ timeout_ms = 2000 })
+      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    end,
+  },
 }
--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,72 +1,26 @@
 return {
-	-- {
-	-- 	"catppuccin/nvim",
-	-- 	lazy = false,
-	-- 	name = "catppuccin",
-	-- 	priority = 1000,
-	-- 	enabled = true,
-	-- 	config = function()
-	-- 		require("catppuccin").setup({
-	-- 			integrations = {
-	-- 				cmp = true,
-	-- 				gitsigns = true,
-	-- 				nvimtree = true,
-	-- 				treesitter = true,
-	-- 				notify = true,
-	-- 				mini = {
-	-- 					enabled = true,
-	-- 					indentscope_color = "",
-	-- 				},
-	-- 			},
-	-- 			flavour = "mocha", -- Cambia entre latte, frappe, macchiato, mocha
-	-- 		})
-	-- 		vim.cmd.colorscheme("catppuccin")
-	-- 	end,
-	-- },
-	-- {
-	-- 	"yorumicolors/yorumi.nvim",
-	-- 	lazy = false,
-	-- 	name = "yorumi",
-	-- 	priority = 1000,
-	-- 	config = function()
-	-- 		-- vim.cmd.colorscheme("yorumi")
-	-- 	end,
-	-- },
-	-- {
-	-- 	"folke/tokyonight.nvim",
-	-- 	lazy = false,
-	-- 	priority = 1000,
-	-- 	opts = {},
-	-- 	config = function()
-	-- 		vim.g.tokyonight_style = "storm"
-	-- 		vim.cmd([[colorscheme tokyonight]])
-	-- 	end,
-	-- },
-	{
-		"akinsho/horizon.nvim",
-		version = "*",
-		opts = {
-			plugins = {
-				cmp = true,
-				indent_blankline = true,
-				nvim_tree = true,
-				telescope = true,
-				which_key = true,
-				barbar = true,
-				notify = true,
-				symbols_outline = true,
-				neo_tree = true,
-				gitsigns = true,
-				crates = true,
-				hop = true,
-				navic = true,
-				quickscope = true,
-				flash = true,
-			},
-		},
-		config = function()
-			require("horizon").setup({})
-			vim.cmd.colorscheme("horizon")
-		end,
-	},
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+    enabled = true,
+    config = function()
+      require("catppuccin").setup({
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          treesitter = true,
+          notify = true,
+          mini = {
+            enabled = true,
+            indentscope_color = "",
+          },
+        },
+        flavour = "mocha", -- Cambia entre latte, frappe, macchiato, mocha
+      })
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
 }
