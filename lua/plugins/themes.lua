5d6d8d0de2b33cc3382b51271a7973045d1fc619
erickGoJi <erickgomezlol96@gmail.com>
2025-08-27 17:52:23 -0600

    [feat] Update theme to catppuccin-mocha

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,26 +1,31 @@
 return {
-  {
-    "catppuccin/nvim",
-    lazy = false,
-    name = "catppuccin",
-    priority = 1000,
-    enabled = true,
-    config = function()
-      require("catppuccin").setup({
-        integrations = {
-          cmp = true,
-          gitsigns = true,
-          nvimtree = true,
-          treesitter = true,
-          notify = true,
-          mini = {
-            enabled = true,
-            indentscope_color = "",
-          },
-        },
-        flavour = "mocha", -- Cambia entre latte, frappe, macchiato, mocha
-      })
-      vim.cmd.colorscheme("catppuccin")
-    end,
-  },
+	{
+		"catppuccin/nvim",
+		lazy = false,
+		name = "catppuccin",
+		priority = 1000,
+		enabled = true,
+		config = function()
+			require("catppuccin").setup({
+				integrations = {
+					cmp = true,
+					gitsigns = true,
+					nvimtree = true,
+					treesitter = true,
+					notify = true,
+					mini = {
+						enabled = true,
+						indentscope_color = "",
+					},
+				},
+				flavour = "mocha", -- Cambia entre latte, frappe, macchiato, mocha
+				background = { -- :h background
+					light = "latte",
+					dark = "mocha",
+				},
+				transparent_background = false,
+			})
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
 }
