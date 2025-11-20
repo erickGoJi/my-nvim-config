d7ffd2cc9f98aee5321e7aedae4c87af8d78a08e
erickGoJi <erickgomezlol96@gmail.com>
2025-11-20 11:56:11 -0600

    [feature] Update themes configuration adding oasis theme

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -31,7 +31,7 @@ return {
 				},
 				dap = true,
 			})
-			vim.cmd.colorscheme("catppuccin")
+			-- vim.cmd.colorscheme("catppuccin")
 
 			sign("DapBreakpoint", { text = "●", texthl = "DapBreakpoint", linehl = "", numhl = "" })
 			sign("DapBreakpointCondition", { text = "●", texthl = "DapBreakpointCondition", linehl = "", numhl = "" })
@@ -43,5 +43,35 @@ return {
 		lazy = false,
 		priority = 1000,
 		opts = {},
+	},
+	{
+		"uhs-robert/oasis.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("oasis").setup({
+				dark_style = "mirage", -- Style to use when vim.o.background is "dark"
+				light_style = "day", -- Style to use when vim.o.background is "light"
+				style = nil, -- Optional: Set a single style to disable auto-switching (e.g., "lagoon", "desert")
+				use_legacy_comments = false, -- Uses the legacy comment color from desert.vim for the "desert" style only (a bright sky blue)
+
+				-- Text styling - disable individual styles if you like
+				styles = {
+					bold = true, -- Enable bold text (keywords, functions, etc.)
+					italic = true, -- Enable italics (comments, certain keywords)
+					underline = true, -- Enable underlined text (matching words)
+					undercurl = true, -- Enable undercurl for diagnostics/spelling
+					strikethrough = true, -- Enable strikethrough text (deprecations)
+				},
+
+				-- Display options
+				transparent = false, -- Set to true for transparent backgrounds (bye bye theme backgrounds)
+				terminal_colors = true, -- Apply Oasis colors to Neovim's built-in terminal
+
+				palette_overrides = {}, -- Override colors in specific palettes
+				highlight_overrides = {}, -- Override specific highlight groups
+			})
+			vim.cmd.colorscheme("oasis-mirage")
+		end,
 	},
 }
